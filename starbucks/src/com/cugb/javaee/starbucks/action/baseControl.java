15439b60ab90d1c79c6249516b2ff58962d65a1e
Chen Huan <2679330388@qq.com>
2020-11-12 19:22:56 +0800

    陈欢的第二次提交

--- starbucks/src/com/cugb/javaee/starbucks/action/baseControl.java
+++ starbucks/src/com/cugb/javaee/starbucks/action/baseControl.java
@@ -2,6 +2,8 @@ package com.cugb.javaee.starbucks.action;
 
 import javax.servlet.http.HttpServlet;
 
-public abstract class baseControl extends HttpServlet {
+import org.apache.log4j.Logger;
 
+public abstract class baseControl extends HttpServlet {
+	protected Logger logger = Logger.getLogger(getClass());
 }
